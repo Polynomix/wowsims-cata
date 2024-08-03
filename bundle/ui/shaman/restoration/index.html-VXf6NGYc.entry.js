aa16f978d2b0c39f82457bef5e2a2f4d1518b345
Polynomix <133067357+Polynomix@users.noreply.github.com>
2024-08-03 18:55:34 +0000

    Deploying to gh-pages from @ Polynomix/wowsims-cata@54bb4d92d14372d04e5a158f720df2302c22bf56 🚀

---
+++ bundle/ui/shaman/restoration/index.html-VXf6NGYc.entry.js
@@ -0,0 +1,1 @@
+import"../../../index-CxS5KVR3.chunk.js";import{P as s,T as t}from"../../../detailed_results-BnSf1ELV.chunk.js";import{S as n,P as e}from"../../../preset_utils-CXIkIYSo.chunk.js";import{R as o}from"../../../sim-h5xPKv9o.chunk.js";import"../../../inputs-DhFYNXw_.chunk.js";const r=new n,i=new e(s.RestorationShaman,r);r.raid.setPlayer(t.nextEventID(),0,i),new o(document.body,i);
